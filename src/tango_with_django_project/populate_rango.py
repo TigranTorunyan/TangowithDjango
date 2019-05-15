0138f5fee157d2dceaf9dfafe38a18c3af2e6b63
Tigran <tigrantorunyan@gmail.com>
2019-05-15 17:09:32 +0400

    Adding  class CategoryAdmin to customize the Admin Interface

--- src/tango_with_django_project/populate_rango.py
+++ src/tango_with_django_project/populate_rango.py
@@ -12,28 +12,28 @@ def populate():
 # Then we will create a dictionary of dictionaries for our categories.
 # This might seem a little bit confusing, but it allows us to iterate
 # through each data structure, and add the data to our models.
-
+    
     python_pages = [
     {'title': 'Official Python Tutorial',
-    'url':'http://docs.python.org/3/tutorial/'},
+    'url':'http://docs.python.org/3/tutorial/', "views": 32 },
     {'title':'How to Think like a Computer Scientist',
-    'url':'http://www.greenteapress.com/thinkpython/'},
+    'url':'http://www.greenteapress.com/thinkpython/', "views": 16},
     {'title':'Learn Python in 10 Minutes',
-    'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    'url':'http://www.korokithakis.net/tutorials/python/', "views": 8} ]
 
     django_pages = [
     {'title':'Official Django Tutorial',
-    'url':'https://docs.djangoproject.com/en/2.0/intro/tutorial01/'},
+    'url':'https://docs.djangoproject.com/en/2.0/intro/tutorial01/', "views": 32},
     {'title':'Django Rocks',
-    'url':'http://www.djangorocks.com/'},
+    'url':'http://www.djangorocks.com/', "views": 16},
     {'title':'How to Tango with Django',
-    'url':'http://www.tangowithdjango.com/'} ]
+    'url':'http://www.tangowithdjango.com/', "views": 8} ]
 
     other_pages = [
     {'title':'Bottle',
-    'url':'http://bottlepy.org/docs/dev/'},
+    'url':'http://bottlepy.org/docs/dev/', "views": 32},
     {'title':'Flask',
-    'url':'http://flask.pocoo.org'} ]
+    'url':'http://flask.pocoo.org', "views": 16} ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
     'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
