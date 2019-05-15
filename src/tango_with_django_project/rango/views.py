4a87c08d6abca6cd82a856cc5fd87bed302664f8
Tigran <tigrantorunyan@gmail.com>
2019-05-15 14:11:13 +0400

    Improving relations between models templates and views: Data driven page, Creating details

--- src/tango_with_django_project/rango/views.py
+++ src/tango_with_django_project/rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rango.models import Category
 # Create your views here.
 
 from django.http import HttpResponse
@@ -7,11 +7,16 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]    
+
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
     #return HttpResponse("Rango says Hello!! <br/> <a href='/rango/about/'>About</a> </br>")
  
 
