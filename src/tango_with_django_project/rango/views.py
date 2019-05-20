831e6cd505f8804b6df1fb3d3a670f15bdf43fca
Tigran <tigrantorunyan@gmail.com>
2019-05-20 16:38:54 +0400

    Logging out

--- src/tango_with_django_project/rango/views.py
+++ src/tango_with_django_project/rango/views.py
@@ -6,6 +6,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
 from django.http import HttpResponse
@@ -180,3 +181,13 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rango/')
